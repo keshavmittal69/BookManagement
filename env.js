861203a4f08ef4fec7ac53b6ea15097c940cf79c
Natasha38 <93899968+Natasha38@users.noreply.github.com>
2023-12-04 18:14:37 +0530

    Delete env.js

--- env.js
+++
@@ -1,9 +0,0 @@
-module.exports.constants = {
-    SERVER_PORT: 3002,
-}
-
-module.exports.dbconstants = {
-    MONGODB_CONNECTION_URL: "mongodb://localhost:27017/",
-    DATABASE: "Bookstore",
-    COLLECTION: "bookshelf",
-}
